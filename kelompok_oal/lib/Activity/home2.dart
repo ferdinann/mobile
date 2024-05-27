55f34ce4e429de539564b66f107eac6ed870ac20
JunaldoSulivan <221110376@students.mikroskil.ac.id>
2024-05-27 21:01:46 +0700

    Update home2.dart

--- kelompok_oal/lib/Activity/home2.dart
+++ kelompok_oal/lib/Activity/home2.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:kelompok_oal/Activity/aboutus.dart';
-import 'package:kelompok_oal/Activity/community.dart';
-import 'package:kelompok_oal/Activity/course.dart';
-import 'package:kelompok_oal/Activity/dropDown.dart';
-import 'package:kelompok_oal/Activity/elearning.dart';
-import 'package:kelompok_oal/Activity/profile.dart';
-import 'package:kelompok_oal/provider/Switch.dart';
-import 'package:kelompok_oal/provider/obscure.dart';
+import 'package:tugas_kelompok/Activity/aboutus.dart';
+import 'package:tugas_kelompok/Activity/community.dart';
+import 'package:tugas_kelompok/Activity/course.dart';
+import 'package:tugas_kelompok/Activity/dropDown.dart';
+import 'package:tugas_kelompok/Activity/elearning.dart';
+import 'package:tugas_kelompok/Activity/profile.dart';
+import 'package:tugas_kelompok/provider/Switch.dart';
+import 'package:tugas_kelompok/provider/obscure.dart';
 import 'package:provider/provider.dart';
 
 class MyHome2 extends StatefulWidget {
@@ -28,6 +28,7 @@ class _MyHome2State extends State<MyHome2> {
     MySnbt()
   ];
   var selectedDDB = null;
+  bool isBannerVisible = true;
 
   @override
   void initState() {
@@ -55,95 +56,88 @@ class _MyHome2State extends State<MyHome2> {
     var DarkMode = Provider.of<Mode>(context);
     return Scaffold(
       appBar: AppBar(
-          elevation: 8,
-          shadowColor: Colors.grey,
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Provider.of<serchClick>(context, listen: false)
-                      .toogleVisible();
-                },
-                icon: Icon(Search.searchClick ? Icons.close : Icons.search)),
-            PopupMenuButton(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  child: ListTile(
-                    title: Text(
-                      'About Us',
+        elevation: 8,
+        shadowColor: Colors.grey,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Provider.of<serchClick>(context, listen: false).toogleVisible();
+            },
+            icon: Icon(Search.searchClick ? Icons.close : Icons.search),
+          ),
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: ListTile(
+                  title: Text(
+                    'About Us',
+                    style: TextStyle(
+                      color: DarkMode.enableDarkMode ? Colors.white : Colors.black,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AboutUsPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              PopupMenuItem(
+                child: Row(
+                  children: [
+                    Text(
+                      'Dark Mode',
                       style: TextStyle(
-                        color: DarkMode.enableDarkMode
-                            ? Colors.white
-                            : Colors.black,
+                        color: DarkMode.enableDarkMode ? Colors.white : Colors.black,
                       ),
                     ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AboutUsPage(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                PopupMenuItem(
-                  child: Row(
-                    children: [
-                      Text(
-                        'Dark Mode',
-                        style: TextStyle(
-                          color: DarkMode.enableDarkMode
-                              ? Colors.white
-                              : Colors.black,
-                        ),
-                      ),
-                      Spacer(),
-                      Switch(
-                        value: DarkMode.enableDarkMode,
-                        onChanged: (value) {
-                          DarkMode.enableDarkMode = value;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                PopupMenuItem(
-                  child: ListTile(
-                    title: Text(
-                      'Logout',
-                      style: TextStyle(
-                        color: DarkMode.enableDarkMode
-                            ? Colors.white
-                            : Colors.black,
-                      ),
+                    Spacer(),
+                    Switch(
+                      value: DarkMode.enableDarkMode,
+                      onChanged: (value) {
+                        DarkMode.enableDarkMode = value;
+                      },
                     ),
-                    onTap: () {
-                      Navigator.popUntil(context, (route) => route.isFirst);
-                    },
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ],
-          title: Search.searchClick
-              ? Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    //color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextField(
-                    //style: TextStyle(color: Colors.black),
-                    controller: _serchBar,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.only(bottom: 10, left: 15),
-                      hintText: "Search...",
-                      //hintStyle: TextStyle(color: Colors.black)
+              ),
+              PopupMenuItem(
+                child: ListTile(
+                  title: Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: DarkMode.enableDarkMode ? Colors.white : Colors.black,
                     ),
                   ),
-                )
-              : Text('LiteBrain')),
+                  onTap: () {
+                    Navigator.popUntil(context, (route) => route.isFirst);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+        title: Search.searchClick
+            ? Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextField(
+                  controller: _serchBar,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(bottom: 10, left: 15),
+                    hintText: "Search...",
+                  ),
+                  onChanged: updateSearchResults,
+                ),
+              )
+            : Text('LiteBrain'),
+      ),
       drawer: Drawer(
         child: Column(
           children: [
@@ -152,14 +146,16 @@ class _MyHome2State extends State<MyHome2> {
                 accountName: Text("Ferdinan"),
                 accountEmail: Text("ferdinantag8@gmail.com"),
                 currentAccountPicture: CircleAvatar(
-                    child: Container(
-                        height: 90,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/profile.jpeg'),
-                          ),
-                        ))),
+                  child: Container(
+                    height: 90,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assets/profile.jpeg'),
+                      ),
+                    ),
+                  ),
+                ),
                 decoration: BoxDecoration(color: Colors.blue),
               ),
               onTap: () {
@@ -215,92 +211,125 @@ class _MyHome2State extends State<MyHome2> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: ListView(children: [
-          if (Search.searchClick) ...[
-            Wrap(
-              spacing: 8.0,
-              runSpacing: 4.0,
-              children: filteredData.map((item) {
-                return Chip(
-                  label: Text(item),
-                  onDeleted: () {
+      body: Column(
+        children: [
+          if (isBannerVisible)
+            MaterialBanner(
+              content: Text('Selamat Datang Kembali! Ayo, Lanjutkan Proses Belajar!.'),
+              backgroundColor: Colors.yellow[100],
+              actions: [
+                TextButton(
+                  onPressed: () {
                     setState(() {
-                      filteredData.remove(item);
+                      isBannerVisible = false;
                     });
                   },
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-          ],
-          const Padding(
-            padding: EdgeInsets.only(bottom: 30, left: 10),
-            child: Text(
-              'LiteBrain',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.blueAccent),
-            ),
-          ),
-          const Text(
-              'Persiapkan dirimu dari sekarang. Dimulai dari belajar skill yang dibutuhkan oleh startup dan industri saat ini. Yuk,tingkatkan skillmu sekarang juga.'),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                DropdownButton(
-                  value: selectedDDB,
-                  hint: const Text("Product"),
-                  items: const [
-                    DropdownMenuItem(
-                      value: "Pemrograman",
-                      child: Text(
-                        'Pemrograman',
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 'Data science',
-                      child: Text(
-                        'Data science',
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 'Cyber security',
-                      child: Text(
-                        'Cyber security',
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
-                    DropdownMenuItem(
-                      value: 'SNBT',
-                      child: Text(
-                        'SNBT',
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
-                  ],
-                  onChanged: (value) {
-                    setState(() {
-                      selectedDDB = value!;
-                    });
+                  child: Text('Dismiss'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyCourse()),
+                    );
                   },
+                  child: Text('See Course'),
                 ),
               ],
             ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: ListView(
+                children: [
+                  if (Search.searchClick) ...[
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      children: filteredData.map((item) {
+                        return Chip(
+                          label: Text(item),
+                          onDeleted: () {
+                            setState(() {
+                              filteredData.remove(item);
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 30, left: 10),
+                    child: Text(
+                      'LiteBrain',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                  const Text(
+                    'Persiapkan dirimu dari sekarang. Dimulai dari belajar skill yang dibutuhkan oleh startup dan industri saat ini. Yuk, tingkatkan skillmu sekarang juga.',
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        DropdownButton(
+                          value: selectedDDB,
+                          hint: const Text("Product"),
+                          items: const [
+                            DropdownMenuItem(
+                              value: "Pemrograman",
+                              child: Text(
+                                'Pemrograman',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Data science',
+                              child: Text(
+                                'Data science',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Cyber security',
+                              child: Text(
+                                'Cyber security',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: 'SNBT',
+                              child: Text(
+                                'SNBT',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              selectedDDB = value!;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  selectedDDB == null
+                      ? dropDown[0]
+                      : selectedDDB == "Data science"
+                          ? dropDown[2]
+                          : selectedDDB == "Cyber security"
+                              ? dropDown[1]
+                              : selectedDDB == "Pemrograman"
+                                  ? dropDown[3]
+                                  : dropDown[4],
+                ],
+              ),
+            ),
           ),
-          selectedDDB == null
-              ? dropDown[0]
-              : selectedDDB == "Data science"
-                  ? dropDown[2]
-                  : selectedDDB == "Cyber security"
-                      ? dropDown[1]
-                      : selectedDDB == "Pemrograman"
-                          ? dropDown[3]
-                          : dropDown[4],
-        ]),
+        ],
       ),
     );
   }
