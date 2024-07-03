5752b056c09ec9e63c763e96268ce815c00b13bd
Ferdinan <ferdinantag8@gmail.com>
2024-07-03 20:54:50 +0700

    membuat screen E-Learning

--- kelompok_oal/lib/Activity/course.dart
+++ kelompok_oal/lib/Activity/course.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kelompok_oal/Activity/test.dart';
+import 'package:intl/intl.dart';
 
 class MyCourse extends StatefulWidget {
   const MyCourse({super.key});
@@ -9,101 +10,148 @@ class MyCourse extends StatefulWidget {
 }
 
 class _MyCourseState extends State<MyCourse> {
-  // Simpan persentase jalan belajar untuk setiap materi
+  // Simpan persentase progress belajar untuk setiap materi
   Map<String, double> learningProgress = {
-    'Data Science': 50.0, // contoh persentase jalan belajar untuk Data Science
-    'Mobile Developer':
-        30.0, // contoh persentase jalan belajar untuk Mobile Developer
+    'Data Science': 50.0, // contoh progress belajar untuk Data Science
+    'Mobile Developer': 30.0, // contoh progress belajar untuk Mobile Developer
   };
+
+  // Simpan deadline untuk setiap materi
+  Map<String, DateTime?> deadlines = {
+    'Data Science': null,
+    'Mobile Developer': null,
+  };
+
+  Future<void> _selectDate(BuildContext context, String course) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: deadlines[course] ?? DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != deadlines[course]) {
+      setState(() {
+        deadlines[course] = DateTime(picked.year, picked.month, picked.day,
+            deadlines[course]?.hour ?? 0, deadlines[course]?.minute ?? 0);
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context, String course) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(deadlines[course] ?? DateTime.now()),
+    );
+    if (picked != null) {
+      setState(() {
+        final date = deadlines[course] ?? DateTime.now();
+        deadlines[course] = DateTime(
+            date.year, date.month, date.day, picked.hour, picked.minute);
+      });
+    }
+  }
+
+  String _formatRemainingTime(DateTime deadline) {
+    final duration = deadline.difference(DateTime.now());
+    if (duration.isNegative) {
+      return 'Deadline Passed!';
+    }
+
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes % 60;
+    final seconds = duration.inSeconds % 60;
+
+    return '$hours hours $minutes minutes $seconds seconds';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Courses'),
+        title: const Text('My Courses'),
+        //backgroundColor: Color.fromARGB(255, 255, 255, 255),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
         children: [
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyTest()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              child: Card(
-                shadowColor: Colors.grey,
-                margin: EdgeInsets.all(20),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image(
-                        image: AssetImage('assets/images/data.jpeg'),
-                        height: 130,
-                        width: 200,
-                      ),
-                      Text(
-                        'Data Science',
-                      ),
-                      SizedBox(height: 10),
-                      // Tampilkan bar persentase jalan belajar
-                      LinearProgressIndicator(
-                        value: learningProgress['Data Science']! / 100,
-                        backgroundColor: Colors.grey[300],
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        '${learningProgress['Data Science']}% Complete',
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyTest()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              child: Card(
-                shadowColor: Colors.grey,
-                margin: EdgeInsets.all(20),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image(
-                        image: AssetImage('assets/images/mobile.jpeg'),
-                        height: 130,
-                        width: 200,
-                      ),
-                      Text(
-                        'Mobile Developer',
-                      ),
-                      SizedBox(height: 10),
-                      // Tampilkan bar persentase jalan belajar
-                      LinearProgressIndicator(
-                        value: learningProgress['Mobile Developer']! / 100,
-                        backgroundColor: Colors.grey[300],
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        '${learningProgress['Mobile Developer']}% Complete',
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
+          _buildCourseCard(context, 'Data Science'),
+          _buildCourseCard(context, 'Mobile Developer'),
         ],
+      ),
+    );
+  }
+
+  Widget _buildCourseCard(BuildContext context, String course) {
+    final deadline = deadlines[course];
+    final formattedDeadline = deadline != null
+        ? DateFormat('MMM d, yyyy HH:mm').format(deadline)
+        : 'No Deadline Chosen!';
+
+    String imagePath;
+    if (course == 'Mobile Developer') {
+      imagePath = 'assets/images/mobile.jpeg';
+    } else {
+      imagePath = 'assets/images/cyber.jpeg';
+    }
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MyTest()));
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(imagePath,
+                  height: 130, width: double.infinity, fit: BoxFit.cover),
+              const SizedBox(height: 10),
+              Text(course,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              LinearProgressIndicator(
+                value: learningProgress[course]! / 100,
+                backgroundColor: Colors.grey[300],
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+              ),
+              const SizedBox(height: 5),
+              Text('${learningProgress[course]}% Complete',
+                  style: TextStyle(fontSize: 16)),
+              const SizedBox(height: 10),
+              Text(
+                deadline == null
+                    ? 'No Deadline Chosen!'
+                    : 'Time Remaining: ${_formatRemainingTime(deadline)}',
+                style: TextStyle(color: Colors.red, fontSize: 16),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                formattedDeadline,
+                style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.calendar_today, color: Colors.teal),
+                    onPressed: () => _selectDate(context, course),
+                    tooltip: 'Set Deadline Date',
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.access_time, color: Colors.teal),
+                    onPressed: () => _selectTime(context, course),
+                    tooltip: 'Set Deadline Time',
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- kelompok_oal/lib/Activity/elearning.dart
+++ kelompok_oal/lib/Activity/elearning.dart
@@ -1,84 +1,173 @@
 import 'package:flutter/material.dart';
-import 'package:kelompok_oal/Activity/home.dart';
-import 'package:kelompok_oal/Activity/profile.dart';
+import 'package:kelompok_oal/provider/learning_provider.dart';
+import 'package:provider/provider.dart';
+import 'learning_card.dart';
 
 class MyLearning extends StatefulWidget {
-  const MyLearning({super.key});
+  const MyLearning({Key? key}) : super(key: key);
 
   @override
   State<MyLearning> createState() => _MyLearningState();
 }
 
 class _MyLearningState extends State<MyLearning> {
+  TextEditingController _searchController = TextEditingController();
+  List<LearningPath> _filteredLearningPaths = [];
+  bool _isSearchActive = false;
+  List<String> _categories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      final learningProvider = context.read<LearningProvider>();
+      learningProvider.loadLearningPaths();
+      setState(() {
+        _categories = _getCategories(learningProvider.learningPaths);
+      });
+    });
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  List<String> _getCategories(List<LearningPath>? learningPaths) {
+    final Set<String> categories = {};
+    for (var path in learningPaths ?? []) {
+      categories.add(path.title);
+    }
+    return categories.toList();
+  }
+
+  void _onSearchChanged() {
+    final learningProvider = context.read<LearningProvider>();
+    final searchQuery = _searchController.text.toLowerCase();
+
+    setState(() {
+      _filteredLearningPaths = learningProvider.learningPaths.where((path) {
+        return path.title.toLowerCase().contains(searchQuery);
+      }).toList();
+    });
+  }
+
+  void _onChipSelected(String category) {
+    _searchController.text = category;
+    _onSearchChanged();
+  }
+
+  void _toggleSearchBar(bool isActive) {
+    setState(() {
+      _isSearchActive = isActive;
+      if (!_isSearchActive) {
+        _searchController.clear();
+        _filteredLearningPaths.clear();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final learningProvider = context.watch<LearningProvider>();
+    final learningPaths = _searchController.text.isEmpty
+        ? learningProvider.learningPaths
+        : _filteredLearningPaths;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('E-Learning'),
+        title: const Text('Daftar E-Learning Yang Ada'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => MyProfile()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              child: const Card(
-                shadowColor: Colors.grey,
-                margin: EdgeInsets.all(20),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image(
-                        image: AssetImage('assets/images/data.jpeg'),
-                        height: 130,
-                        width: 200,
-                      ),
-                      Text(
-                        'Data Science',
-                      ),
-                    ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _searchController,
+                  onTap: () => _toggleSearchBar(true),
+                  onChanged: (text) => _toggleSearchBar(true),
+                  style: TextStyle(
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Theme.of(context).colorScheme.onSurface
+                        : Theme.of(context).textTheme.bodyText1?.color,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Search...',
+                    hintStyle: TextStyle(
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? Theme.of(context)
+                              .colorScheme
+                              .onSurface
+                              .withOpacity(0.6)
+                          : Theme.of(context).textTheme.bodyText2?.color,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Theme.of(context).brightness == Brightness.dark
+                        ? Theme.of(context).colorScheme.surface
+                        : Colors.white,
+                    prefixIcon: Icon(Icons.search),
                   ),
                 ),
-              ),
+                if (_isSearchActive)
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: _categories.map((category) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                          child: ActionChip(
+                            label: Text(
+                              category,
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSecondary),
+                            ),
+                            labelStyle: TextStyle(color: Colors.white),
+                            backgroundColor:
+                                Theme.of(context).colorScheme.secondary,
+                            onPressed: () => _onChipSelected(category),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+              ],
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyHome()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              child: Card(
-                shadowColor: Colors.grey,
-                margin: EdgeInsets.all(20),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image(
-                        image: AssetImage('assets/images/mobile.jpeg'),
-                        height: 130,
-                        width: 200,
-                      ),
-                      Text(
-                        'Mobile Developer',
-                      ),
-                    ],
+          Expanded(
+            child: learningPaths.isEmpty
+                ? const Center(child: Text('None'))
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      childAspectRatio: 0.8,
+                      children: learningPaths.map((path) {
+                        return LearningCard(
+                          title: path.title,
+                          imagePath: path.imagePath,
+                          studentCount: path.studentCount,
+                          topicCount: path.topicCount,
+                          materialCount: path.materialCount,
+                          rating: path.rating.toStringAsFixed(2),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                ),
-              ),
-            ),
-          )
+          ),
         ],
       ),
-      floatingActionButton:
-          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
     );
   }
 }
